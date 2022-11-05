2d690973b223c017feb8dedaf7caf308e84a66c8
Cristobal Leon <admin@iMac-de-Cristobal.local>
2022-11-05 19:56:16 -0300

    actualizando

--- index.js
+++ index.js
@@ -5,8 +5,28 @@ import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import linkRouter from './routes/link.route.js';
 import redirectRouter from './routes/redirect.route.js';
+import cors from "cors";
 
 const app = express();
+
+const whiteList = [process.env.ORIGIN1];
+   
+app.use(
+  cors({
+      origin: function (origin, callback) {
+          console.log("😲😲😲 =>", origin);
+          if (!origin || whiteList.includes(origin)) {
+              return callback(null, origin);
+          }
+          return callback(
+              "Error de CORS origin: " + origin + " No autorizado!"
+          );
+      },
+      credentials: true,
+  })
+);
+
+
 
 app.use(express.json());
 app.use(cookieParser());
