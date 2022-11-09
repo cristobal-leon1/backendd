d35a3ff1277617b2a54c3f92bfd4807a3b4abbd3
Cristobal Leon <admin@iMac-de-Cristobal.local>
2022-11-08 22:06:50 -0300

    crud cotizaciones

--- utils/tokenManager.js
+++ utils/tokenManager.js
@@ -20,8 +20,9 @@ export const generateRefreshToken = (uid, res) => {
         const refreshToken = jwt.sign({ uid}, process.env.JWT_REFRESH, { expiresIn });
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-            secure: !(process.env.MODO === "developer"),
-            expires: new Date(Date.now() + expiresIn * 1000)
+            //secure: !(process.env.MODO === "developer"),
+            expires: new Date(Date.now() + expiresIn * 1000),
+            sameSite: "none",
         })
     } catch(error) {
         console.log(error);
